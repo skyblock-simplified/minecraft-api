536c1ef8c271dcc2ca507f8c9447ca8a8331130a
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2025-09-17 04:26:58 -0400

    Fix Map.Entry typing

--- src/main/java/dev/sbs/minecraftapi/skyblock/island/crimson_isle/TrophyFishing.java
+++ src/main/java/dev/sbs/minecraftapi/skyblock/island/crimson_isle/TrophyFishing.java
@@ -3,27 +3,26 @@ package dev.sbs.minecraftapi.skyblock.island.crimson_isle;
 import dev.sbs.api.collection.concurrent.Concurrent;
 import dev.sbs.api.collection.concurrent.ConcurrentMap;
 import dev.sbs.api.stream.pair.Pair;
+import dev.sbs.api.stream.pair.PairOptional;
 import dev.sbs.minecraftapi.MinecraftApi;
 import dev.sbs.minecraftapi.skyblock.model.TrophyFish;
 import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.Optional;
 
 @Getter
 public class TrophyFishing {
 
     private final @NotNull ConcurrentMap<String, ConcurrentMap<TrophyFish.Tier, Integer>> fish;
     private final int totalCaught;
-    private final @NotNull Optional<Pair<String, TrophyFish.Tier>> lastCaught;
+    private final @NotNull PairOptional<String, TrophyFish.Tier> lastCaught;
 
     public TrophyFishing(@NotNull ConcurrentMap<String, Object> trophy_fish) {
         this.totalCaught = (int) trophy_fish.removeOrGet("total_caught", 0);
 
-        this.lastCaught = trophy_fish.getOptional("last_caught")
+        this.lastCaught = PairOptional.of(trophy_fish.getOptional("last_caught")
             .map(String::valueOf)
             .map(value -> value.split("/"))
-            .map(parts -> Pair.of(parts[0], TrophyFish.Tier.valueOf(parts[1])));
+            .map(parts -> Pair.of(parts[0], TrophyFish.Tier.valueOf(parts[1]))));
 
         this.fish = MinecraftApi.getRepositoryOf(TrophyFish.class)
             .stream()
