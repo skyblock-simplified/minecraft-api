066e1b2f66eba14baf5d229b496b3a9f2e0c830b
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2025-10-02 01:53:28 -0400

    Add accessory model, json file still being worked on

---
+++ src/main/java/dev/sbs/minecraftapi/skyblock/model/Accessory.java
@@ -0,0 +1,67 @@
+package dev.sbs.minecraftapi.skyblock.model;
+
+import dev.sbs.api.collection.concurrent.ConcurrentList;
+import dev.sbs.api.data.Model;
+import dev.sbs.minecraftapi.MinecraftApi;
+import dev.sbs.minecraftapi.client.mojang.profile.MojangProperty;
+import dev.sbs.minecraftapi.skyblock.Rarity;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Optional;
+
+public interface Accessory extends Model {
+
+    @NotNull String getId();
+
+    @NotNull String getName();
+
+    @NotNull Optional<String> getDescription();
+
+    @NotNull Rarity getRarity();
+
+    @NotNull Source getSource();
+
+    @NotNull Optional<MojangProperty> getSkin();
+
+    @NotNull Item.Attributes getAttributes();
+
+    @NotNull Optional<Family> getFamily();
+
+    @NotNull ConcurrentList<Substitute> getStats();
+
+    default @NotNull Item getItem() {
+        return MinecraftApi.getRepositoryOf(Item.class)
+            .findFirstOrNull(Item::getId, this.getId());
+    }
+
+    interface Family {
+
+        @NotNull String getId();
+
+        int getRank();
+
+    }
+
+    enum Source {
+
+        COLLECTION,
+        NPC,
+        EVENT,
+        MOB_DROP,
+        SLAYER,
+        QUEST,
+        DUNGEON,
+        MISCELLANEOUS,
+        RIFT
+
+    }
+
+    interface Substitute {
+
+        @NotNull String getId();
+
+        double getValue();
+
+    }
+
+}
