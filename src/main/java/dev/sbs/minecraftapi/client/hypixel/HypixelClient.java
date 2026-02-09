0ae6b2b26c9ff2b02b45684144c9c5b526d63e0c
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2026-02-09 09:08:42 -0500

    Remove limited response header caching, difference in memory is negligible, but debugging and logging improvement is significant

--- src/main/java/dev/sbs/minecraftapi/client/hypixel/HypixelClient.java
+++ src/main/java/dev/sbs/minecraftapi/client/hypixel/HypixelClient.java
@@ -3,10 +3,8 @@ package dev.sbs.minecraftapi.client.hypixel;
 import dev.sbs.api.SimplifiedApi;
 import dev.sbs.api.client.Client;
 import dev.sbs.api.client.exception.ClientErrorDecoder;
-import dev.sbs.api.client.response.CFCacheStatus;
 import dev.sbs.api.collection.concurrent.Concurrent;
 import dev.sbs.api.collection.concurrent.ConcurrentMap;
-import dev.sbs.api.collection.concurrent.ConcurrentSet;
 import dev.sbs.api.stream.pair.Pair;
 import dev.sbs.minecraftapi.client.hypixel.exception.HypixelClientException;
 import dev.sbs.minecraftapi.client.hypixel.request.HypixelEndpoints;
@@ -19,16 +17,6 @@ public final class HypixelClient extends Client<HypixelEndpoints> {
 
     public HypixelClient() {
         super("api.hypixel.net");
-    }
-
-    @Override
-    protected @NotNull ConcurrentSet<String> configureResponseHeaders() {
-        return Concurrent.newUnmodifiableSet(
-            CFCacheStatus.HEADER_KEY,
-            "RateLimit-Limit",
-            "RateLimit-Remaining",
-            "RateLimit-Reset"
-        );
     }
 
     @Override
