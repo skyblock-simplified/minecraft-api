ba5bc596eefbda7405db9c514a8b47f86e49a2f5
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2026-02-14 03:47:37 -0500

    Add missing constructors

--- src/main/java/dev/sbs/minecraftapi/nbt/exception/NbtException.java
+++ src/main/java/dev/sbs/minecraftapi/nbt/exception/NbtException.java
@@ -2,7 +2,9 @@ package dev.sbs.minecraftapi.nbt.exception;
 
 import dev.sbs.minecraftapi.exception.MinecraftException;
 import dev.sbs.minecraftapi.nbt.NbtFactory;
+import org.intellij.lang.annotations.PrintFormat;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * {@link NbtException NbtExceptions} are thrown when the {@link NbtFactory} class is unable<br>
@@ -10,12 +12,24 @@ import org.jetbrains.annotations.NotNull;
  */
 public class NbtException extends MinecraftException {
 
-    public NbtException(@NotNull Exception exception) {
-        super(exception);
+    public NbtException(@NotNull Throwable cause) {
+        super(cause);
     }
 
     public NbtException(@NotNull String message) {
         super(message);
+    }
+
+    public NbtException(@NotNull Throwable cause, @NotNull String message) {
+        super(cause, message);
+    }
+
+    public NbtException(@NotNull @PrintFormat String message, @Nullable Object... args) {
+        super(String.format(message, args));
+    }
+
+    public NbtException(@NotNull Throwable cause, @NotNull @PrintFormat String message, @Nullable Object... args) {
+        super(String.format(message, args), cause);
     }
 
 }
