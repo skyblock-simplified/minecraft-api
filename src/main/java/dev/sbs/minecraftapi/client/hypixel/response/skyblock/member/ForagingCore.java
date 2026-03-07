11cc80f538b1784f59a250328511adcd2dc60a82
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2026-03-07 03:05:11 -0500

    Add ForagingCore

---
+++ src/main/java/dev/sbs/minecraftapi/client/hypixel/response/skyblock/member/ForagingCore.java
@@ -0,0 +1,37 @@
+package dev.sbs.minecraftapi.client.hypixel.response.skyblock.member;
+
+import com.google.gson.annotations.SerializedName;
+import dev.sbs.api.collection.concurrent.Concurrent;
+import dev.sbs.api.collection.concurrent.ConcurrentList;
+import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Optional;
+
+@Getter
+public class ForagingCore {
+
+    @SerializedName("current_daily_effect")
+    private Optional<String> currentLotteryEffect = Optional.empty();
+    @SerializedName("current_daily_effect_last_changed")
+    private int lotteryEffectLastChanged;
+
+    // Whispers
+    @SerializedName("forest_whispers")
+    private int remainingForestWhispers;
+    @SerializedName("forest_whispers_spent")
+    private int spentForestWhispers;
+
+    // Daily Logs
+    @SerializedName("daily_trees_cut")
+    private int dailyTreesCut;
+    @SerializedName("daily_trees_cut_day")
+    private int dailyTreesCutDay;
+    @SerializedName("daily_gifts")
+    private int dailyGifts;
+    @SerializedName("daily_log_cut")
+    private @NotNull ConcurrentList<String> dailyLogCut = Concurrent.newList();
+    @SerializedName("daily_log_cut_day")
+    private int dailyLogCutDay;
+
+}
