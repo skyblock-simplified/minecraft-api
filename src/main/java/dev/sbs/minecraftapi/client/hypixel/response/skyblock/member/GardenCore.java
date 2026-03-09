621862f363133cbec87ceb02a5b91d7ef8a5c48b
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2026-03-08 20:26:04 -0400

    Add garden player data class

---
+++ src/main/java/dev/sbs/minecraftapi/client/hypixel/response/skyblock/member/GardenCore.java
@@ -0,0 +1,21 @@
+package dev.sbs.minecraftapi.client.hypixel.response.skyblock.member;
+
+import com.google.gson.annotations.SerializedName;
+import dev.sbs.api.collection.concurrent.Concurrent;
+import dev.sbs.api.collection.concurrent.ConcurrentList;
+import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
+
+@Getter
+public class GardenCore {
+
+    private int copper;
+    @SerializedName( "larva_consumed")
+    private int larvaConsumed;
+    @SerializedName("analyzed_greenhouse_crops")
+    private @NotNull ConcurrentList<String> analyzedGreenhouseCrops = Concurrent.newList();
+    @SerializedName("discovered_greenhouse_crops")
+    private @NotNull ConcurrentList<String> discoveredGreenhouseCrops = Concurrent.newList();
+
+
+}
