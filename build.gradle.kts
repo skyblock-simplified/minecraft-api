7a66c278a8f3ba275f44c69c7d2b5c2b446070f8
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2026-03-25 07:28:47 -0400

    Gradle and claude overhaul

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
-    id("java")
+    id("java-library")
 }
+
+group = "dev.sbs"
+version = "0.1.0"
 
 repositories {
     mavenCentral()
@@ -9,53 +12,23 @@ repositories {
 }
 
 dependencies {
-    // IntelliJ Annotations
-    implementation(group = "org.jetbrains", name = "annotations", version = "24.0.1")
-
-    // Resource Checker Annotations
-    implementation(group = "dev.sbs", name = "simplified-annotations", version = "1.0.4")
-    annotationProcessor(group = "dev.sbs", name = "simplified-annotations", version = "1.0.4")
+    // Simplified Annotations
+    annotationProcessor(libs.simplified.annotations)
 
     // Lombok Annotations
-    compileOnly(group = "org.projectlombok", name = "lombok", version = "1.18.30")
-    annotationProcessor(group = "org.projectlombok", name = "lombok", version = "1.18.30")
-    testCompileOnly(group = "org.projectlombok", name = "lombok", version = "1.18.30")
-    testAnnotationProcessor(group = "org.projectlombok", name = "lombok", version = "1.18.30")
+    compileOnly(libs.lombok)
+    annotationProcessor(libs.lombok)
+    testCompileOnly(libs.lombok)
+    testAnnotationProcessor(libs.lombok)
 
     // Tests
-    testImplementation(group = "org.hamcrest", name = "hamcrest", version = "2.2")
-    testImplementation(group = "org.junit.jupiter", name = "junit-jupiter-api", version = "5.10.0")
-    testRuntimeOnly(group = "org.junit.jupiter", name = "junit-jupiter-engine", version = "5.10.0")
-    testImplementation(group = "com.h2database", name = "h2", version = "2.2.224")
-
-    // Logging
-    implementation(group = "org.apache.logging.log4j", name = "log4j-core", version = "2.25.3")
-    implementation(group = "org.apache.logging.log4j", name = "log4j-slf4j-impl", version = "2.25.3")
-
-    // Deserialization
-    implementation(group = "com.google.code.gson", name = "gson", version = "2.10.1")
-    implementation(group = "org.yaml", name = "snakeyaml", version = "2.0")
-
-    // Database
-    implementation(group = "org.mariadb.jdbc", name = "mariadb-java-client", version = "3.1.4")
-    implementation(group = "org.ehcache", name = "ehcache", version = "3.10.8")
-    implementation(group = "org.hibernate", name = "hibernate-hikaricp", version = "5.6.15.Final") // 7.0.2.Final
-    implementation(group = "org.hibernate", name = "hibernate-jcache", version = "5.6.15.Final")
-
-    // Api Clients
-    implementation(group = "io.github.openfeign", name = "feign-gson", version = "13.6")
-    implementation(group = "io.github.openfeign", name = "feign-httpclient", version = "13.6")
+    testImplementation(libs.hamcrest)
+    testImplementation(libs.junit.jupiter.api)
+    testRuntimeOnly(libs.junit.jupiter.engine)
 
     // Projects
-    implementation(optionalProject(":api", "com.github.skyblock-simplified:api:master-SNAPSHOT"))
+    api("dev.sbs:api:0.1.0")
 }
-
-fun Project.optionalProject(path: String, fallbackDependency: String): Any =
-    if (project.rootProject.findProject(path) != null) {
-        project(path)
-    } else {
-        fallbackDependency
-    }
 
 tasks {
     withType<JavaCompile> {
