433c56caf17d9ef2371d3ecf8773c8eb8111a65c
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2026-03-31 11:43:11 -0400

    New melody harp using Capture annotation

---
+++ src/main/java/dev/sbs/minecraftapi/client/hypixel/response/skyblock/member/foraging/MelodyHarp.java
@@ -0,0 +1,38 @@
+package dev.sbs.minecraftapi.client.hypixel.response.skyblock.member.foraging;
+
+import com.google.gson.annotations.SerializedName;
+import dev.sbs.api.collection.concurrent.Concurrent;
+import dev.sbs.api.collection.concurrent.ConcurrentMap;
+import dev.sbs.api.io.gson.Capture;
+import dev.sbs.minecraftapi.skyblock.date.SkyBlockDate;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Optional;
+
+@Getter
+public class MelodyHarp {
+
+    @SerializedName("claimed_talisman")
+    private boolean talismanClaimed;
+    @SerializedName("selected_song")
+    private @NotNull Optional<String> selectedSong = Optional.empty();
+    @SerializedName("selected_song_epoch")
+    private @NotNull SkyBlockDate.RealTime selectedSongTimestamp = new SkyBlockDate.RealTime(0);
+    @Capture(filter = "^song_")
+    private @NotNull ConcurrentMap<String, Song> songs = Concurrent.newMap();
+
+    @Getter
+    @NoArgsConstructor
+    public static class Song {
+
+        @SerializedName("best_completion")
+        private int bestCompletion;
+        private int completions;
+        @SerializedName("perfect_completions")
+        private int perfectCompletions;
+
+    }
+
+}
